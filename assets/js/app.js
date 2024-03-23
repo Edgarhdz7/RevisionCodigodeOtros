3acd2fee0825c2be72f316b010c908da0e69ee03
Edgar Hernandez <edgar.hdz.glez7@gmail.com>
2024-03-22 19:10:37 -0600

    Archivos agregados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+// Correccion: Seleccionando elementos con clases y un ID
+const $n = document.querySelector('.name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    console.log(data);
+    // Correccion: comillas inversas para interpolar variables
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // Correccion: Accediendo al elemento con la clase "name"
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
+// Invocando la función displayUser con el nombre de usuario "stolinski"
 displayUser('stolinski').catch(handleError);
